64b32938d224c35cb730e3d932bc08c416b338f1
Prasanth Anbalagan <panbalag@redhat.com>
2022-06-23 18:16:22 +0000

    Include code for prediction and flask application

---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+import json
+from kafka import KafkaConsumer
+
+
+KAFKA_BOOTSTRAP_SERVER = os.getenv('KAFKA_BOOTSTRAP_SERVER')
+KAFKA_SECURITY_PROTOCOL = os.getenv('KAFKA_SECURITY_PROTOCOL', 'SASL_SSL')
+KAFKA_SASL_MECHANISM = os.getenv('KAFKA_SASL_MECHANISM','PLAIN')
+KAFKA_USERNAME = os.getenv('KAFKA_USERNAME')
+KAFKA_PASSWORD = os.getenv('KAFKA_PASSWORD')
+KAFKA_CONSUMER_GROUP = 'ml-consumer'
+KAFKA_TOPIC = os.getenv('KAFKA_TOPIC')
+SAMPLE_SIZE = os.getenv('SAMPLE_SIZE')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+
+
+def main():
+    # Normally, we'd never want to lose a message,
+    # but we want to ignore old messages for this demo, so we set
+    # enable_auto_commit=False
+    # auto_offset_reset='latest' (Default)
+    # This has the effect of starting from the last message.
+
+    consumer = KafkaConsumer(KAFKA_TOPIC,
+                             group_id=KAFKA_CONSUMER_GROUP,
+                             bootstrap_servers=[KAFKA_BOOTSTRAP_SERVER],
+                             security_protocol=KAFKA_SECURITY_PROTOCOL,
+                             sasl_mechanism=KAFKA_SASL_MECHANISM,
+                             sasl_plain_username=KAFKA_USERNAME,
+                             sasl_plain_password=KAFKA_PASSWORD,
+                             api_version_auto_timeout_ms=30000,
+                             request_timeout_ms=450000)
+    print(f'Subscribed to "{KAFKA_BOOTSTRAP_SERVER}" consuming topic "{KAFKA_TOPIC}"...')
+    f = open("batch.csv", "a")
+    f.write("timestamp,value\n")
+    count = 0
+    try:
+        print(f'Consuming messages from topic "{KAFKA_TOPIC}"...')
+        for record in consumer:
+            count += 1
+            msg = record.value.decode('utf-8')
+            topic = record.topic
+            item = json.loads(msg)
+            timestamp = item["timestamp"]
+            value = item["value"]
+            line = timestamp + "," + value + "\n"
+            f.write(line)
+            if (count == SAMPLE_SIZE):
+                print("Running prediction on collected data...")
+                predict(WEBHOOK_URL)
+                count = 0
+                f.close()
+                os.remove("batch.csv")
+                f = open("batch.csv", "a")
+                f.write("timestamp,value\n")
+                print("Continuing to collect next batch of data...")
+    finally:
+        print("Closing consumer...")
+        consumer.close()
+
+if __name__ == '__main__':
+    main()
---
+++ prediction.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import joblib
+from fbprophet import Prophet
+import requests
+
+
+def run_prediction(url):
+    m = joblib.load('model.pkl')
+    future = m.make_future_dataframe(periods=100, freq='H')
+    forecast = m.predict(future)
+    df = pd.read_csv("batch.csv")
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df = df.set_index('timestamp').resample('H').mean().reset_index()
+    df = df.reset_index()[['timestamp', 'value']].rename({'timestamp': 'ds', 'value': 'y'}, axis = 'columns')
+    result = pd.concat([df.set_index('ds')['y'], forecast.set_index('ds')[['yhat','yhat_lower','yhat_upper']]], axis=1)
+    result['error'] = result['y'] - result['yhat']
+    result['uncertainty'] = result['yhat_upper'] - result['yhat_lower']
+    result['anomaly'] = result.apply(lambda x: 'Yes' if(np.abs(x['error']) > 1.5*x['uncertainty']) else 'No', axis = 1)
+    newdf = result.loc[result['anomaly'] == 'Yes']
+    for index, row in newdf.iterrows():
+        msg = f'Found anomaly at time "{index}" with usage of "{row["y"]}" bytes'
+        print("Message:")
+        headers = {
+            'Content-type': 'application/json',
+        }
+        json_data = {
+            'text': msg,
+        }
+        response = requests.post(url, headers=headers, json=json_data)
